3d108896a62fcf04a09cdbe14982e27a977488df
Wei Du <wei_du@viarail.ca>
2017-09-27 13:09:22 -0400

    bug fix - fixed line break issue

--- FormBuilder.php
+++ FormBuilder.php
@@ -3,14 +3,25 @@
 class FromBuilder extends FPDF
 {
 function Leftcol($a,$b=35,$c=10){
-$this->SetFont('Arial','B',12);
+$this->SetFont('Arial','B',10);
 $this->SetTextColor(0,0,0);
 $this->SetFillColor(255,255,255);
-$this->Cell($b,$c,$a.":",1,0,'R',1);
+$x = $this->GetX();
+$y = $this->GetY();
+
+if(strlen($a) < 20){
+	$this->Cell($b,$c,"\r\n".$a,1,0,'R',1);
+}
+else{
+$this->SetFont('Arial','B',8);
+$this->MultiCell($b,5,$a,1,0,'R',1);
+$this->setXY($x+35,$y);
+}
 }
 
 function Textcol($a,$b=1,$c=10){
 $this->SetFont('Times');
+$this->SetFontSize(10);
 $this->SetTextColor(0,0,0);
 $this->SetFillColor(255,255,255);
 if($b==1){
